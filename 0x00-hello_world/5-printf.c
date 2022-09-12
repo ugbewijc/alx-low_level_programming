291cb3860d634841caf3c875204da626569eb19f
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-09-12 02:24:34 -0700

    5-printf.c intital commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main() - Program Entry point
+ * Args: takes no args
+ *
+ * Des: use printf() to print exactly
+ *  " with proper grammar, but the outcome is a piece of art"
+ *  function to print to stdout
+ * Return: return 0(success)
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
