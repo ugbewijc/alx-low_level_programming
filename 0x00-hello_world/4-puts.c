6e54287d455f6ff5d8c74d6b0f68ad47a8d46e56
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-09-12 02:11:25 -0700

    4-puts.c initial commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Program Entry point
+ * Arg - it takes no arg.
+ * Return: Returns 0 (success)
+ * Des: C program that prints exactly
+ * "Programming is like building a multilingual puzzle"
+ *  use puts() func to print string to stdout
+ *
+ */
+
+int main(void) /*main func*/
+
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+
+}
