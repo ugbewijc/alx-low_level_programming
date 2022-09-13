99124c9003b3a7862e4596d6bc8c03eda3711454
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-09-13 14:54:18 -0700

    0-putchar.c initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  main -  Program Entry Point
+ *  Agr: takes no agurments
+ *  Return: returns 0(success)
+ *  Desc: C program that prints _putchar, followed by a new line.
+ *
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+ ALX SE C progamming Language tasks on functions and nested loop
