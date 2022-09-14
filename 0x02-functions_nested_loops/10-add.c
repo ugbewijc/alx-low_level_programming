846c8eb15d24c62e098c4c2d333a7c34562f6303
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-09-14 03:07:10 -0700

    10-add.c initial commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ *
+ * @a: function parameter, datatype integer
+ * @b: function parameter, datatype integer
+ *
+ * Return: sume of the two arg
+ *
+ *
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
