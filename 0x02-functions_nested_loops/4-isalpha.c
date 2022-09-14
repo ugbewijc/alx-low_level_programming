778dae03da28c71552aba6377bcff1a16cf86ee8
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-09-13 17:13:57 -0700

    4-isalpha.c initial commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if character alphabet
+ *
+ * @c: function agr for charater, datatype interger
+ *
+ * Return: returns 1 if c is a letter, lowercase or uppercase,
+ *          otherwise, return 0
+ *
+ *
+ */
+
+int _isalpha(int c)
+{
+	if ((c >= 97) && (c <= 122))
+	|| ((c >= 97) && (c <= 122))
+	{
+		return (1);
+	}
+	return (0);
+}
