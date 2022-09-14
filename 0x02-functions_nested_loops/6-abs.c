8537f15096c0b2ee2ff2d1a1912effff1a04f01d
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-09-13 18:06:25 -0700

    6-abs.c initial commit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @n: function parameter, datatype integer
+ *
+ * Return: returns the absolute value passed to the function
+ *
+ *
+ */
+
+int _abs(int n)
+{
+	return (n >= 0 ? n : n * -1);
+}
