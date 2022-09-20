e98e96659c9cd132caf3e6ca49862084a0324862
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-09-20 15:50:36 -0700

    2-strlen.c initial commit

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen - return the length of a string
+ *
+ * @s: function parameter, datattype char, string to check
+ *
+ * Return: length of @s
+ *
+ *
+ */
+
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
