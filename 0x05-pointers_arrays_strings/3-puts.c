d4f8111c1755d97ff1ad1a638f91f06546dd3f57
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-09-20 16:26:17 -0700

    3-puts.c initial commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout, using _putchar()
+ *
+ * @str:  function parameter, datatype char, inputted string to print
+ *
+ *
+ * Return: void
+ *
+ *
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] > -1; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
