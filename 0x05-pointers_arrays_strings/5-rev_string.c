ef825d6877b78e0691b6d14db36d4b428c0dd057
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-09-20 17:07:02 -0700

    5-rev_string.c initial commit

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ *
+ * @s: function parameter, datatype string, input string to be reversed
+ *
+ *
+ */
+
+void rev_string(char *s)
+{
+	char tmp;
+	int i, str_len;
+
+	str_len = strlen(s) - 1;
+
+	for (i = 0; i < (str_len / 2); i++)
+	{
+		tmp = s[i];
+		s[i] = s[str_len];
+		s[str_len--] = tmp;
+	}
+}
