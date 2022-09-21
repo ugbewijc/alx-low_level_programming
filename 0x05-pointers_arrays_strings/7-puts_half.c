77077278a93c5552e0db7056dfaf4b51c6b0104d
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-09-20 17:43:10 -0700

    7-puts_half.c intital commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of the string
+ *
+ * @str: function parameter, datatype string, input string
+ *
+ * Return: void
+ *
+ *
+ */
+
+void puts_half(char *str)
+{
+	int i = strlen(str);
+
+	for (i /= 2; str[i] >= 0; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
