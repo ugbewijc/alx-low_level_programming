1440f5fbc958b84fa69ae56327b54e8d453b4f26
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-09-20 17:25:30 -0700

    6-puts2.c initial commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * puts2 -  prints every other character of a string, starting with the
+ *          first character, followed by a new line
+ *
+ * @str: function parameter, datatype string
+ *
+ *
+ */
+void puts2(char *str)
+{
+	int i, len;
+
+	len = strlen(str);
+
+	for (i = 0; i < lent; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
