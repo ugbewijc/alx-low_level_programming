07fa6a8ae812138843abbc4b0f78030a4f4360eb
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-11-04 12:37:19 -0700

    updated 1-create_file script

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -35,11 +35,19 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content)
+	if (!text_content)
 	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-			;
-		rwr = write(fd, text_content, nletters);
+		return (-1);
+	}
+	for (nletters = 0; text_content[nletters]; nletters++)
+	{
+	}
+
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
+	{
+		return (-1);
 	}
 	close(fd);
 	return (1);
