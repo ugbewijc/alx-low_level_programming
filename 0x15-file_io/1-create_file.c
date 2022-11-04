d3d3dabeafce98418ed25f243ec75634bee47747
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-11-04 12:23:14 -0700

    1-create_file.c initial commmit

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * create_file - creates a file
+ *
+ * @filename:function parameter, filename
+ *
+ * @text_content: function Parameter, file content
+ *
+ *
+ * Return: 1 on success,
+ *          -1 on failure (file can not be; created, written
+ *          write “fails”, etc…)
+ *          -1 if filename is NULL
+ *
+ *
+ *
+ * The created file must have those permissions: rw-------.
+ * If the file already exists, do not change the permissions.
+ * if the file already exists, truncate it
+ * if text_content is NULL create an empty file*
+ *
+ **/
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd, nletters, rwr;
+
+	if (!filename)
+	{
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+	{
+		return (-1);
+	}
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+		rwr = write(fd, text_content, nletters);
+	}
+	close(fd);
+	return (1);
+}
