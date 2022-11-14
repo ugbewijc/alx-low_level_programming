f273fc04a4a5920444fbf1630609afeb910f2a84
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-11-14 09:32:16 -0800

    5-print_numbers.c initial commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- Program Entry Point
+ *
+ * Arg: takes no argument
+ *
+ * Return: returns 0(success)
+ *
+ *
+ **/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
