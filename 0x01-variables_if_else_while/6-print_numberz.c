9bbd286a057671af310a24dcfd21e9712ab65131
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-11-14 09:41:43 -0800

    6-print_numberz.c initial commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- Program Entry Point
+ *
+ * Arg: takes no argument
+ *
+ * Return: returns 0(success)
+ *
+ *
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
