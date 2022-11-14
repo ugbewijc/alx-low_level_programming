b25f4aa3b36725a30a419016641f1b4c800f6bff
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-11-14 02:27:51 -0800

    2-print_alphabet.c intital commit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main- Program Entry Point
+ *
+ * Arg: takes no argument
+ *
+ * Return: returns 0(success)
+ *
+ *
+ **/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
