682c1221cd6b5f05ebbf9ccf6a559d3f37293de4
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-11-23 02:06:45 -0800

    3-print_numbers.c initial commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers, from 0 to 9, followed by a new line.
+ *
+ *
+ * Return: void
+ *
+ **/
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
