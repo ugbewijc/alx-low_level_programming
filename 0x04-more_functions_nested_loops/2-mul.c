0e31b5cbf1fba4e0b43be1f704519b049f5d524b
John Chioma Ugbewi <ugbewijc@gmail.com>
2022-11-23 01:57:32 -0800

    2-mul.c initial commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ *
+ * @a: function parameter, first input
+ *
+ * @b: function parameter, second input
+ *
+ *
+ * Return: return results
+ *
+ **/
+int mul(int a, int b)
+{
+	return (a * b);
+}
